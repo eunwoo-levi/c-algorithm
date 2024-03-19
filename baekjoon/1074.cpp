78b0b4f81c1d7d92ece52ac4356dc6840e12982d
eunwoo-levi <eunwoo1341@gmail.com>
2024-03-19 22:24:30 +0900

    1074 - 재귀 (Z)

---
+++ baekjoon/1074.cpp
@@ -0,0 +1,24 @@
+// 1074 - 재귀 (Z)
+#include <bits/stdc++.h>
+using namespace std;
+int func(int n, int r, int c){
+    if(n==0)    return 0;
+
+    int half = 1 <<(n-1); // == 2^(n-1)     - 비트마스킹
+
+    if(r < half && c<half)  return func(n-1,r,c);
+    if(r < half && c>=half) return half*half + func(n-1,r,c-half);
+    if(r >= half && c<half) return 2*half*half + func(n-1,r-half,c);
+    return  3*half*half + func(n-1,r-half,c-half);
+
+
+}
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int n,r,c;
+
+    cin >> n >> r >> c;
+
+    cout << func(n,r,c);
+}
