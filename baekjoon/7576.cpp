0e9f8948e855de965773d3cc675c666ff6b4e8e4
eunwoo-levi <eunwoo1341@gmail.com>
2024-03-19 15:39:53 +0900

    // 7576 - bfs   , 여러개 bfs를 동시에 한번에 동작하는 경우

---
+++ baekjoon/7576.cpp
@@ -0,0 +1,60 @@
+// 7576 - bfs   , 여러개 bfs를 동시에 한번에 동작하는 경우
+#include <bits/stdc++.h>
+using namespace std;
+
+int n,m,x,y, res;
+int A[1001][1001], visited[1001][1001];
+const int dy[4] = {-1,0,1,0},dx[4] = {0,1,0,-1};
+queue<pair<int,int>> Q;
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> m >> n;
+
+    int flag = 0;
+
+    for(int i=0; i<n;i++){
+        for(int j=0; j<m;j++){
+            cin >> A[i][j];
+            if(A[i][j]==1){
+                visited[i][j] = 1;
+                Q.push({i,j});
+            }
+            if(A[i][j]==0)  flag=1;
+        }
+    }
+
+    if(flag==0){
+        cout << "0";
+        return 0;
+    }
+
+    while(!Q.empty()){
+        tie(y,x) = Q.front();
+        Q.pop();
+
+        for(int i=0; i<4; i++){
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+
+            if(ny<0||ny>=n||nx<0||nx>=m)   continue;
+            if(visited[ny][nx] || A[ny][nx] == -1)  continue;
+            visited[ny][nx] = visited[y][x] + 1;
+            res = max(res, visited[ny][nx]);
+            Q.push({ny,nx});
+        }
+    }
+
+    for(int i=0; i<n;i++){
+        for(int j=0; j<m;j++){
+            if(A[i][j]==0 && visited[i][j] == 0){
+                cout << "-1";
+                return 0;
+            }
+        }
+    }
+
+
+    cout << res-1;
+    return 0;
+}
